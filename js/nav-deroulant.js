c75df37d5fc3356dbdfa257ba493cdc5d6bdcb3a
Rinzler <119778544+TheRinzler65@users.noreply.github.com>
2024-09-13 15:51:26 +0200

    update color and navbar

--- js/nav-deroulant.js
+++ js/nav-deroulant.js
@@ -3,22 +3,26 @@ document.addEventListener('DOMContentLoaded', function () {
 
     dropdowns.forEach(dropdown => {
         dropdown.addEventListener('click', function (event) {
+            // Empêche la propagation de l'événement de clic pour les liens
             if (event.target.closest('.dropdown-content a')) {
                 return;
             }
 
             event.preventDefault();
 
+            // Ferme tous les autres menus déroulants
             dropdowns.forEach(otherDropdown => {
                 if (otherDropdown !== dropdown) {
                     otherDropdown.classList.remove('open');
                 }
             });
 
+            // Bascule l'état du menu déroulant cliqué
             this.classList.toggle('open');
         });
     });
 
+    // Ferme tous les menus déroulants lorsqu'on clique en dehors
     document.addEventListener('click', function (event) {
         if (!event.target.closest('.dropdown')) {
             dropdowns.forEach(dropdown => dropdown.classList.remove('open'));
